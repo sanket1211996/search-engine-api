cdc0567ff9776478e81e5a1c5ca605f9df4fb0b5
Darwante, Sanket Atmaram <sdarwant@iu.edu>
2022-09-11 04:21:48 -0400

    base mini test

--- securities.py
+++ securities.py
@@ -1,5 +1,5 @@
 class Securities:
-    def __init__(self, security_id, cusip, sedol, isin, ric, bloomberg, bbg, symbol, root_symbol, bb_yellow, spn) -> None:
+    def __init__(self, security_id, cusip, sedol, isin, ric, bloomberg, bbg, symbol, root_symbol, bb_yellow, spn, priority) -> None:
         self.security_id = security_id
         self.cusip = cusip
         self.sedol = sedol
@@ -11,3 +11,4 @@ class Securities:
         self.root_symbol = root_symbol
         self.bb_yellow = bb_yellow
         self.spn = spn
+        self.priority = priority
